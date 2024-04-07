c76d1bd91ca3ae6bf8296ccfd76704bc8195352f
Xenren <kamren@infinitejest.net>
2024-04-06 17:58:00 -0700

    finalized application packaged into an App class for easy portablity/reuseability in other applications if required updated readme to reflect proper usage

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Overview
 
-Basic application for changing video sizes with an extremely simple GUI.
+Basic application built for personal use for changing video sizes with an extremely simple GUI.
+
+# Uses:
+
+The `./app` contains two files
+1. `app.py` - the primary application that loops through a pre-defined set of sizes
+    - was created for use by a non-coder; thus I didn't want the user to have to consider pixel sizes.
+    - simply edit the `target_width` array to add sizes
+2. `custom_downscaler.py` - secondary version that resizes a single video to a specfied pixel height
+
+    *NOTE: the code for this version needs to be updated to match `app.py` but it is fully functional.*
 
 # Before Use:
 
@@ -15,3 +25,18 @@ resized_pil = pilim.resize(newsize[::-1], Image.LANCZOS)
 ``` 
 
 and it will work just fine. `ANTIALIAS is depricated and moviepy has not changed their code yet.
+
+# To Build Executable:
+
+Make sure you have pyinstaller
+```sh
+pip install pyinstaller
+```
+
+Navigate to the `./app` directory and run:
+
+```sh
+pyinstaller --onefile --windowed downscaler.py
+```
+
+***NOTE: The executable file will only work on the same operating system on which it was created.***
---
+++ app/app.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+import threading
+from moviepy.editor import VideoFileClip
+
+class App:
+    def __init__(self, root: tk.Tk):
+        self.root = root
+        self.setup_gui()
+
+    def setup_gui(self):
+        self.root.title("Video Downscaling")
+        self.progress = ttk.Progressbar(self.root, orient="horizontal", length=400, mode='determinate')
+        self.progress.pack(pady=20)
+        tk.Button(self.root, text="Select Video", command=self.select_and_process_video).pack(pady=20)
+
+    def update_progress(self, progress: float):
+        self.progress['value'] = progress
+        self.root.update_idletasks()
+
+    def downscale_video(self, filepath: str):
+        target_widths = [1920, 1280, 854]
+        output_files = []
+        
+        messagebox.showinfo("Downscale Commence", "Start Downscale?")
+        for task_no, target_width in enumerate(target_widths, start=1):
+            clip = VideoFileClip(filepath)
+            target_height = int(target_width / clip.aspect_ratio)
+            extension = filepath.split('.')[-1]
+            save_path = f"{filepath.rsplit('.', 1)[0]}_{target_width}x{target_height}.{extension}"
+            clip.resize(newsize=(target_width, target_height)).write_videofile(save_path, codec="libx264", audio_codec="aac", verbose=False, logger=None) # type: ignore
+            clip.close()
+            output_files.append(save_path)
+
+            self.update_progress((task_no / len(target_widths)) * 100)
+
+        self.root.after(0, messagebox.showinfo, "Success", "Videos downscaled successfully!\n\n" + "\n".join(output_files))
+        self.update_progress(0)
+
+    def select_and_process_video(self):
+        filepath = filedialog.askopenfilename(title="Select a video file", filetypes=(("Video files", "*.mp4* *.avi* *.mov*"), ("All files", "*.*")))
+        if not filepath:
+            return
+
+        threading.Thread(target=self.downscale_video, args=(filepath,), daemon=True).start()
+
+def main():
+    root = tk.Tk()
+    App(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
